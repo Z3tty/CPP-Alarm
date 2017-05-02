718e6c633890dea748066f14bae4b3d33d8db4a4
ZxZ <mattias.langseth98@gmail.com>
2017-05-02 11:10:40 +0200

    Create alarm.cpp

---
+++ src/alarm.cpp
@@ -0,0 +1,36 @@
+#if defined(_WIN32)
+	#include <stdlib.h>
+	#include <stdio.h>
+	#include <stdarg.h>
+	#include <time.h>
+	#include <ctime>
+	#include <windows.h>
+	#include <mmsystem.h>
+
+	int main(int argv, char **argc)
+	{
+		if(!argc[1] || !argc[2] || !argc[3]) throw "CLI Argument error: invoke with 'alarm h m s' ";
+		int hour 	= 	atoi(argc[1]);
+		int min 	= 	atoi(argc[2]);
+		int sec 	= 	atoi(argc[3]);
+		struct {int h; int m; int s;} tm_alarm;
+		tm_alarm.h = hour; tm_alarm.m = min; tm_alarm.s = sec;
+
+		int sleepTime = 5;
+		bool alarm = false;
+		do {
+			::Sleep(sleepTime*1000);
+			time_t time = std::time(0);
+			struct tm *tm_now = localtime(&time);
+			if((tm_alarm.h)		<=		(tm_now->tm_hour)			&&
+			   (tm_alarm.m)		<=		(tm_now->tm_min)			&&
+			   (tm_alarm.s)		<=		(tm_now->tm_min)) {
+				::PlaySound("Path\\To\\Alarm\\Sound", NULL, SND_FILENAME | SND_LOOP);
+				alarm = true;
+				for(int i = 0; i < 100000; i++) printf("ALARM\t ALARM\t ALARM\t ALARM\t ALARM");
+			}
+		}while(!alarm);
+	}
+#else
+	throw "Unsupported operating system";
+#endif
